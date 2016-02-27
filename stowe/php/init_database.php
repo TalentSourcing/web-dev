61b483a63678f3bffed4259893d1204aa453778e
David Lindskog <d.lindskog1@gmail.com>
2016-02-27 11:39:51 -0800

    Using Chuan's init_database file.  Deleted mine, renamed his to init_database.php.  Made some small changes to the file.

--- stowe/php/init_database.php
+++ stowe/php/init_database.php
@@ -1,42 +1,44 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: davidlindskog
- * Date: 2/26/16
- * Time: 3:54 PM
- */
 
 // This file will initialize the mysql database and set up the necessary tables.
 
-function createDb() {
-    $servername = "localhost:8889";
-    $username = "root";
-    $password = "root";
-    $dbname = "talentme_db";
+function init() {
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	$dbname = "talentme_db";
 
-    // Create connection
-    $conn = new mysqli($servername, $username, $password);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 
-    // Create database
-    $sql = "CREATE DATABASE " . $dbname;
-    if ($conn->query($sql) === TRUE) {
-        echo "Database created successfully";
-    } else {
-        echo "Error creating database: " . $conn->error;
+	/* Attempt to select 'talentme_db' as database.  If it does not exist, create it. */
+	if ($conn->select_db($dbname)) {
+        $result = $conn->query("SELECT DATABASE()");
+        $row = $result->fetch_row();
+        printf("Current database is %s.\n", $row[0]);
+        $result->close();
+	}
+    else {
+        echo "No database by the name '$dbname' found. ";
+
+        // Create database
+        $sql = "CREATE DATABASE " . $dbname;
+        if ($conn->query($sql) === TRUE) {
+            echo "New database '$dbname' created successfully. ";
+        } else {
+            echo "Error creating database: " . $conn->error;
+        }
     }
 
     // TODO create tables here ----
 
-
-
-    $conn->close();
+	$conn->close();
 }
 
+init();
 
-createDb();
-
-
+?>
