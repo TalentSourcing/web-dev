b3986c9a6ef5b32849b88ee462f94a51bf981003
David Lindskog <d.lindskog1@gmail.com>
2016-02-27 11:55:18 -0800

    Converted indents to spaces

--- stowe/php/init_database.php
+++ stowe/php/init_database.php
@@ -2,26 +2,26 @@
 
 // This file will initialize the mysql database and set up the necessary tables.
 
-function init() {
-	$servername = "localhost";
-	$username = "root";
-	$password = "root";
-	$dbname = "talentme_db";
+function init () {
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "talentme_db";
 
-	// Create connection
-	$conn = mysqli_connect($servername, $username, $password);
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-	/* Attempt to select 'talentme_db' as database.  If it does not exist, create it. */
-	if ($conn->select_db($dbname)) { // select_db returns true if found, false if not found
+    /* Attempt to select 'talentme_db' as database.  If it does not exist, create it. */
+    if ($conn->select_db($dbname)) { // select_db returns true if found, false if not found
         $result = $conn->query("SELECT DATABASE()");
         $row = $result->fetch_row();
         printf("Current database is %s.\n", $row[0]);
         $result->close();
-	}
+    }
     else { // database $dbname not found, create a new one
         echo "No database by the name '$dbname' found. ";
         // Create database
@@ -35,7 +35,7 @@ function init() {
 
     // TODO create tables here ----
 
-	$conn->close();
+    $conn->close();
 }
 
 init();
