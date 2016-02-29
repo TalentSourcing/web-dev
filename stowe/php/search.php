20bcb674aab4fdd2e65a7a5836292e8785777b4b
myalavarthy <misha.yalavarthy@gmail.com>
2016-02-29 12:01:21 -0800

    search php

--- stowe/php/search.php
+++ stowe/php/search.php
@@ -39,95 +39,59 @@ if(!$button)
     echo "Please submit a valid search.";
 
 else
-{
-    if(strlen($search)<=1)
-    echo "Search term too short";
+    {
+        if(strlen($search)<=1)
+        echo "Search term too short";
 
-else
-{
-    echo "You searched for <b>$search</b> <hr size='1'></br>";
-
-    mysql_connect("localhost","your mysql username","password");
-    mysql_select_db("TalentMeDB");
-
-    $search_exploded = explode (" ", $search);
-
-foreach($search_exploded as $search_each)
-{
-    $x++;
-    if($x==1)
-        $construct .="keywords LIKE '%$search_each%'";
     else
-        $construct .="AND keywords LIKE '%$search_each%'";
+        {
+            echo "You searched for <b>$search</b> <hr size='1'></br>";
 
-}
+            mysql_connect("8888","your mysql username","password");
 
-$construct ="SELECT * FROM TalentMeDB WHERE $construct";
-$run = mysql_query($construct);
+            mysql_select_db("TalentMeDB");
 
-$foundnum = mysql_num_rows($run);
+            $search_exploded = explode (" ", $search);
 
-if ($foundnum==0)
-    echo "Sorry, there are no matching results for <b>$search</b>.</br></br>1.
-    Try a more general search.</br>2. Try different words with similar meaning</br>3. Please check your spelling.";
-else
-{
-    echo "$foundnum results found !<p>";
+            foreach($search_exploded as $search_each)
+                {
+                    $x++;
+                    if($x==1)
+                    $construct .="keywords LIKE '%$search_each%'";
 
-while($runrows = mysql_fetch_assoc($run))
-{
-    $title = $runrows ['title'];
-    $desc = $runrows ['description'];
-    $url = $runrows ['url'];
+                    else
+                        $construct .="AND keywords LIKE '%$search_each%'";
 
-echo "
-<a href='$url'><b>$title</b></a><br>
-$desc<br>
-<a href='$url'>$url</a><p>
-";
+                }
 
-}
-}
+            $construct ="SELECT * FROM TalentMeDB WHERE $construct";
 
-}
-}
+            $run = mysql_query($construct);
 
-*/
+            $foundnum = mysql_num_rows($run);
 
+            if ($foundnum==0)
 
+                echo "Sorry, there are no matching results for <b>$search</b>.</br></br>1.
+                Try a more general search.</br>2. Try different words with similar meaning</br>3. Please check your spelling.";
 
-/*
+                else
+                    {
+                        echo "$foundnum results found !<p>";
 
+                        while($runrows = mysql_fetch_assoc($run))
+                            {
+                                $title = $runrows ['title'];
+                                $desc = $runrows ['description'];
+                                $url = $runrows ['url'];
 
-
-<?php
-if (!empty($_REQUEST['term'])) {
-
-$term = mysql_real_escape_string($_REQUEST['term']);
-
-$sql = "SELECT * FROM liam WHERE Description LIKE '%".$term."%'";
-$r_query = mysql_query($sql);
-
-while ($row = mysql_fetch_array($r_query)){
-echo 'Primary key: ' .$row['PRIMARYKEY'];
-echo '<br /> Code: ' .$row['Code'];
-echo '<br /> Description: '.$row['Description'];
-echo '<br /> Category: '.$row['Category'];
-echo '<br /> Cut Size: '.$row['CutSize'];
-}
-
-}
-?>
-
-
+                                echo "<a href='$url'><b>$title</b></a><br>$desc<br><a href='$url'>$url</a><p>";
+                            }
+                    }
+        }
+    }
 
 */
-
-
-
-
-
-
 
 
 
