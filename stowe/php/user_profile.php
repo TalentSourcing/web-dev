a7bf202cf65dcf83fc4324e797b0f886c10024bd
David Lindskog <d.lindskog1@gmail.com>
2016-02-29 14:34:39 -0800

    updateUser functionality added to user_profile.php

--- stowe/php/user_profile.php
+++ stowe/php/user_profile.php
@@ -18,18 +18,41 @@ class DatabaseInterface {
     function createUser ($user_email, $first_name, $last_name, $password, $linkedin_url, $skills, $occupation, $gender,
                          $profile_img, $objective) {
         $sql = "INSERT INTO UserTable ".
-            " (user_email, first_name, last_name, password, linkedin_url, skills, occupation, gender, profile_img, objective) ".
+            "(user_email, first_name, last_name, password, linkedin_url, skills, occupation, gender, profile_img, objective) ".
             "VALUES ('$user_email', '$first_name', '$last_name', '$password', '$linkedin_url', '$skills', '$occupation',
             '$gender', '$profile_img', '$objective')";
 
         if ($this->conn->query($sql)) {
-            echo "Insert success!";
+            echo "New user insert success!";
         }
         else {
-            echo "Insert Failure: " . $this->conn->error . "<br>";
+            echo "New user insert Failure: " . $this->conn->error . "<br>";
+        }
+    }
+
+    public function updateUser ($user_email, $first_name, $last_name, $password, $linkedin_url, $skills, $occupation, $gender,
+                            $profile_img, $objective) {
+        $sql = "UPDATE UserTable SET ".
+            "first_name='$first_name', ".
+            "last_name='$last_name', ".
+            "password='$password', ".
+            "linkedin_url='$linkedin_url', ".
+            "skills='$skills', ".
+            "occupation='$occupation', ".
+            "gender='$gender', ".
+            "profile_img='$profile_img', ".
+            "objective='$objective' ".
+            "WHERE user_email='$user_email'";
+
+        if ($this->conn->query($sql)) {
+            echo "User profile update success!";
+        }
+        else {
+            echo "User profile update Failure: " . $this->conn->error . "<br>";
         }
     }
 }
 
 $dbi = new DatabaseInterface();
 $dbi->createUser("d.lindskog1@gmail.com", "David", "Lindskog", "password", "", "", "", "", "", "");
+$dbi->updateUser("d.lindskog1@gmail.com", "Bob", "Nelson", "password", "", "", "", "", "", "");
