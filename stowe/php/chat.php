66984600a282ca680ddd695403aa06fc5fe304ac
myalavarthy <misha.yalavarthy@gmail.com>
2016-02-29 15:17:18 -0800

    ...

--- stowe/php/chat.php
+++ stowe/php/chat.php
@@ -11,7 +11,7 @@ public function DatabaseInterface () {
 
 function createGroupChat ($creator_user_email, $group_id){
 
-    $sql = "INSERT INTO ChatLineTable ". "(creator_user_email, group_id)". "VALUES        ('$creator_user_email', '$group_id')";
+    $sql = "INSERT INTO ChatLineTable ". "(creator_user_email, group_id) ". "VALUES        ('$creator_user_email', '$group_id')";
 
     if ($this->conn->query($sql)){
         echo "Insert success!";
@@ -19,4 +19,33 @@ function createGroupChat ($creator_user_email, $group_id){
     else{
         echo "Insert Failure: " . $this->conn->error . "<br>";
     }
+}
+
+
+function createIndividualChat ($creator_user_email, $recipient_user_email){
+
+
+    $sql = "INSERT INTO ChatLineTable ". "(user_email, user)"
+}
+
+
+function send ($user_email, $chat_id, $text_line){
+
+
+}
+
+
+//returns list of chatId’s affiliated with user
+function getChatList ($user_email){
+
+    $sql = "SELECT user_name, chat_id" .
+        "FROM ChatLineTable";
+}
+
+
+//query for chatId, return list of chat lines
+function getChat ($chat_id){
+
+    $sql = "SELECT chat_id, text_line" .
+        "FROM ChatLineTable";
 }
