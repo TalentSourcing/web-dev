0a6fcc3e7a589633d463614504c4bdef4165fbc2
myalavarthy <misha.yalavarthy@gmail.com>
2016-02-29 16:30:13 -0800

    chat.php most functions done

--- stowe/php/chat.php
+++ stowe/php/chat.php
@@ -11,8 +11,6 @@ public function DatabaseInterface () {
 
 function createGroupChat ($creator_user_email, $group_id){
 
-
-
     $sql = "INSERT INTO ChatLineTable ".
         "(id, chat_id, user_email, text_line, time_stamp, group_id) ".
         "VALUES ('', '', $creator_user_email','','',  '$group_id')";
@@ -25,20 +23,48 @@ function createGroupChat ($creator_user_email, $group_id){
     }
 }
 
-
+//naina - please help on completing this function
 function createIndividualChat ($creator_user_email, $recipient_user_email){
 
-    $sql = "INSERT INTO ChatLineTable ".
+$sql = "SELECT user_email " .
+    "FROM UserTable " .
+    "WHERE user_name " .
+    "LIKE '$recipient_user_email'";
+
+
+
+    try{
+
+        $sql = "INSERT INTO ChatLineTable ".
         "(id, chat_id, user_email, text_line, time_stamp, group_id)" .
         "VALUES ('$creator_user_email', )";
+
+        echo "Success.";
+    }
+
+    catch(Exception $e){
+        echo "Error Message" $e->getMessage();
+    }
+
 }
 
 
 function send ($user_email, $chat_id, $text_line){
 
-    $sql = "INSERT INTO ChatLineTable ".
+$time_stamp = date(h.i.sa.d.m.y);
+
+    try{
+
+        $sql = "INSERT INTO ChatLineTable ".
         "(id, chat_id, user_email, text_line, time_stamp, group_id) " .
         "VALUES ('','$chat_id', '$user_email, '$text_line','$time_stamp','')";
+
+        echo "Success.";
+    }
+
+    catch(Exception $e){
+        echo "Error Message" $e->getMessage();
+    }
 
 }
 
