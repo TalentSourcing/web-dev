3396984cc42ec73308f648927ad30ec393749276
BhargaviK6 <Bhargavi.k6@gmail.com>
2016-02-29 16:56:34 -0800

    GroupsPHP Functionality

--- stowe/php/Groups.php
+++ stowe/php/Groups.php
@@ -12,11 +12,30 @@ class DatabaseInterface {
         $this->conn = TalentMeDB::getConnection();
     }
 
-    function createGroupTable ($founderuser_email, $group_name, $about, $desiredSkills, $groupImg)
+    function createGroup ($user_email, $group_name, $about, $desired_skills, $group_img)
     {
         $sql = "INSERT INTO GroupTable ".
-            " (founderuser_email, group_name, about, desiredSkills, groupimg) ".
-            "VALUES ('$founderuser_email', '$group_name', '$about', '$desiredSkills', '$groupImg') ";
+            " (user_email, group_name, group_img , about, desired_skills) ".
+            "VALUES ('$user_email', '$group_name', '$group_img', '$about', '$desired_skills') ";
+
+        try {
+            if ($this->conn->query($sql)) {
+                echo "Success!";
+            }
+        } catch (Exception $e) {
+            echo $e . "<br>";
+        }
+        echo "after if/else";
+    }
+    //this function updates the group details in GroupTable
+    function update ($group_id,$group_name, $about, $desired_skills, $group_img)
+    {
+        $sql = "UPDATE GroupTable SET  ".
+            "group_name='$group_name',".
+            "about='$about',".
+            "desired_skills='$desired_skills',".
+            "group_img='$group_img',".
+            "WHERE (group_id)=('$group_id') ";
 
         try {
             if ($this->conn->query($sql)) {
@@ -28,7 +47,76 @@ class DatabaseInterface {
         echo "after if/else";
     }
 
+    function delete ($group_id)
+    {
+        $sql = "DELETE FROM GroupTable ".
+            " (group_name, about, desired_skills, group_img) ".
+            "WHERE (group_id)=('$group_id') ";
+
+        try {
+            if ($this->conn->query($sql)) {
+                echo "Success!";
+            }
+        } catch (Exception $e) {
+            echo $e . "<br>";
+        }
+        echo "after if/else";
+    }
+    function inviteUser ($user_email,$group_id)
+
+    {
+
+        $applied="Applied";
+        $sql = "INSERT INTO UserGroupTable ".
+            " (user_email, group_id, user_role ) ".
+            "VALUES ('$user_email', '$group_id', $applied) ";
+
+        try {
+            if ($this->conn->query($sql)) {
+                echo "Success!";
+            }
+        } catch (Exception $e) {
+            echo $e . "<br>";
+        }
+        echo "after if/else";
+    }
+    function approveUser ($user_email,$group_id)
+
+    {
+
+        $member="Member";
+        $sql = "UPDATE UserGroupTable SET  ".
+            "user_role='$member'".
+            "WHERE (user_email)=('$user_email') ".
+            "group_id='$group_id'";
+
+        try {
+            if ($this->conn->query($sql)) {
+                echo "Success!";
+            }
+        } catch (Exception $e) {
+            echo $e . "<br>";
+        }
+        echo "after if/else";
+    }
+    function declineUserApplication ($user_email,$group_id)
+    {
+        $sql = "DELETE FROM UserGroupTable ".
+            " (user_group_id,user_email,group_id,user_role) ".
+            "WHERE (user_email)=('$user_email') ".
+            "group_id='$group_id'";
+
+        try {
+            if ($this->conn->query($sql)) {
+                echo "Success!";
+            }
+        } catch (Exception $e) {
+            echo $e . "<br>";
+        }
+        echo "after if/else";
+    }
 }
 
 $dbi = new DatabaseInterface();
-$dbi->createGroupTable("bk@gmail.com", "Group1", "Computer","Java", "img.jpg");
+$dbi->createGroup("bk@gmail.com", "Group1", "Computer","Java", "img.jpg");
+
