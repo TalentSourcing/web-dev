61e914bb712742142cb08873cad98645664204fe
myalavarthy <misha.yalavarthy@gmail.com>
2016-03-03 13:30:55 -0800

    ...

--- stowe/js/home/dashboard/groups/user_decline.js
+++ stowe/js/home/dashboard/groups/user_decline.js
@@ -1,3 +1,17 @@
-delete(){
-var elem = document.getElementById("deleteme");
+
+
+delete1(){
+var elem = document.getElementById("deleteme1");
+elem.parentNode.removeChild(elem);}
+
+delete2(){
+var elem = document.getElementById("deleteme2");
+elem.parentNode.removeChild(elem);}
+
+delete3(){
+var elem = document.getElementById("deleteme3");
+elem.parentNode.removeChild(elem);}
+
+delete4(){
+var elem = document.getElementById("deleteme4");
 elem.parentNode.removeChild(elem);}
