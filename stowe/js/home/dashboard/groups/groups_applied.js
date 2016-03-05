dca41a08634b9d26a1764f32b63ef36438dd1337
David Lindskog <d.lindskog1@gmail.com>
2016-03-05 14:59:41 -0800

    groups_current and groups_applied now passing group_id to group page upon link click

--- stowe/js/home/dashboard/groups/groups_applied.js
+++ stowe/js/home/dashboard/groups/groups_applied.js
@@ -11,29 +11,31 @@ function generateHtml () {
             return null;
         }
 
-        appliedGroups.pending.push({
-            'group_name' : 'Group 3',
-            'group_img' : '',
-            'about' : 'blahblahblha',
-            'desired_skills' : ''
-        });
-
-        appliedGroups.pending.push({
-            'group_name' : 'Group 4',
-            'group_img' : '',
-            'about' : 'oinfosinfoiefneso',
-            'desired_skills' : ''
-        });
+        //appliedGroups.pending.push({
+        //    'group_name' : 'Group 3',
+        //    'group_img' : '',
+        //    'about' : 'blahblahblha',
+        //    'desired_skills' : ''
+        //});
+        //
+        //appliedGroups.pending.push({
+        //    'group_name' : 'Group 4',
+        //    'group_img' : '',
+        //    'about' : 'oinfosinfoiefneso',
+        //    'desired_skills' : ''
+        //});
 
         var groups_list = $('.groups_list');
         appliedGroups.pending.forEach(function (group) {
-            group.about = "About this group!"; // TODO delete
-            group.group_img = "default-placeholder.png"; // TODO delete
+            if (group.about === "") {
+                group.about = "No info...";
+            }
             var list_item =
                 '<li class="groups_list_item">' +
                     '<img class="groups_list_img" src="../../../../image/'+ group.group_img +'" alt="img" />' +
                     '<div class="div_groups_list_text">' +
-                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html">'+ group.group_name +'</a><br>' +
+                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html"' +
+                            'onclick="openGroup('+ group.group_id +')">'+ group.group_name +'</a><br>' +
                         '<label class="label_subtext">'+ group.about +'</label>' +
                     '</div>' +
                     '<div class="list_icon pending">' +
@@ -43,13 +45,15 @@ function generateHtml () {
             groups_list.append(list_item);
         });
         appliedGroups.accepted.forEach(function (group) {
-            group.about = "About this group!"; // TODO delete
-            group.group_img = "default-placeholder.png"; // TODO delete
+            if (group.about === "") {
+                group.about = "No info...";
+            }
             var list_item =
                 '<li class="groups_list_item">' +
                     '<img class="groups_list_img" src="../../../../image/'+ group.group_img +'" alt="img" />' +
                     '<div class="div_groups_list_text">' +
-                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html">'+ group.group_name +'</a><br>' +
+                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html"' +
+                            'onclick="openGroup('+ group.group_id +')">'+ group.group_name +'</a><br>' +
                         '<label class="label_subtext">'+ group.about +'</label>' +
                     '</div>' +
                     '<div class="list_icon accepted">' +
@@ -59,6 +63,17 @@ function generateHtml () {
             groups_list.append(list_item);
         });
     });
+}
+
+function openGroup (group_id) {
+    var groupStorage;
+    if ((groupStorage = JSON.parse(sessionStorage.getItem('group'))) === null) {
+        groupStorage = {
+            'group_id' : ''
+        };
+    }
+    groupStorage.group_id = group_id;
+    sessionStorage.setItem('group', JSON.stringify(groupStorage));
 }
 
 generateHtml();
--- stowe/js/home/dashboard/groups/groups_current.js
+++ stowe/js/home/dashboard/groups/groups_current.js
@@ -15,19 +15,32 @@ function generateHtml () {
 
         var groups_list = $('.groups_list');
         joinedGroups.forEach(function (group) {
-            //group.about = "About this group!";
-            //group.group_img = "default-placeholder.png";
+            if (group.about === "") {
+                group.about = "No info...";
+            }
             var list_item =
                 '<li class="groups_list_item">' +
                     '<img class="groups_list_img" src="../../../../image/'+ group.group_img +'" alt="img" />' +
                     '<div class="div_groups_list_text">' +
-                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html">'+ group.group_name +'</a><br>' +
+                        '<a class="group_name" href="../../../../html/home/dashboard/groups/view_group_external.html"' +
+                            'onclick="openGroup('+ group.group_id +')">'+ group.group_name +'</a><br>' +
                         '<label class="label_subtext">'+ group.about +'</label>' +
                     '</div>' +
                 '</li>';
             groups_list.append(list_item);
         });
     });
+}
+
+function openGroup (group_id) {
+    var groupStorage;
+    if ((groupStorage = JSON.parse(sessionStorage.getItem('group'))) === null) {
+        groupStorage = {
+            'group_id' : ''
+        };
+    }
+    groupStorage.group_id = group_id;
+    sessionStorage.setItem('group', JSON.stringify(groupStorage));
 }
 
 generateHtml();
