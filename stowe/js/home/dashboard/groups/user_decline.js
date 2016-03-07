c4577bfa6058f2374622e4d16990a4c0d3bf17b3
myalavarthy <misha.yalavarthy@gmail.com>
2016-03-07 10:59:01 -0800

    ...

--- stowe/js/home/dashboard/groups/user_decline.js
+++ stowe/js/home/dashboard/groups/user_decline.js
@@ -1,3 +1,5 @@
+'use strict';
+
 
 delete1(){
 var parent = document.getElementById("list_applicants");
@@ -7,13 +9,41 @@ parent.removeChild(child);
 
 
 delete2(){
-var elem = document.getElementById("deleteme2");
-elem.parentNode.removeChild(elem);}
+var parent = document.getElementById("list_applicants");
+var child = document.getElementById("deleteme2");
+parent.removeChild(child);
+}
 
 delete3(){
-var elem = document.getElementById("deleteme3");
-elem.parentNode.removeChild(elem);}
+var parent = document.getElementById("list_applicants");
+var child = document.getElementById("deleteme3");
+parent.removeChild(child);
+}
 
 delete4(){
-var elem = document.getElementById("deleteme4");
-elem.parentNode.removeChild(elem);}
+var parent = document.getElementById("list_applicants");
+var child = document.getElementById("deleteme4");
+parent.removeChild(child);
+}
+
+
+var xmlhttp = new XMLHttpRequest();
+    var response = null;
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var json_str = extractJSONObject(xmlhttp.responseText);
+            console.log(json_str);
+            response = JSON.parse(json_str);
+            if ('error' in response) {
+                console.log(response.error);
+                alert('Error: ' + response.error);
+            }
+            else {
+                alert('Group application success!');
+
+            }
+        }
+    };
+    xmlhttp.open("GET","http://localhost/apply_for_group.php?" + APPLY_FOR_GROUP + "=" + str_rot13(JSON.stringify(request)), true);
+    xmlhttp.send();
+}
